2b9457e648c891afad8bec178db819b77323905f
r <rserrano7@ucmerced.edu>
2025-01-28 17:17:16 -0800

    Update sumo.py
    beginning snippet of how to utilize code functions, will need to be flushed out with advancement of simulation

--- SUMO/sumo.py
+++ SUMO/sumo.py
@@ -1,6 +1,26 @@
 import traci
 import sumolib
 
+# Load the network file
+net = sumolib.net.readNet('path/to/your/network.xml')
+
+# Start the simulation
+sumoBinary = sumolib.checkBinary('sumo')
+traci.start([sumoBinary, "-c", "path/to/your/config.sumocfg"])
+
+# Run the simulation for 1000 steps
+for step in range(1000):
+    traci.simulationStep()
+
+    # Get the position of a vehicle
+    vehicle_id = "vehicle_0"
+    if vehicle_id in traci.vehicle.getIDList():
+        x, y = traci.vehicle.getPosition(vehicle_id)
+        print(f"Vehicle {vehicle_id} is at ({x}, {y})")
+
+# Close the simulation
+traci.close()
+
 #pip install sumolib traci
 
 #only downloaded what i needed here, will need to figure out how this actually works later on 
