c63c070b1ad452d36d78b0c3e613f6b07775d58f
r <rserrano7@ucmerced.edu>
2025-01-28 17:15:32 -0800

    Update gtfs-process.py
    beginning snippet of code that allows reading and processing of GTFS data, which will help with the usage of the real-time code.

--- GTFS/gtfs-process.py
+++ GTFS/gtfs-process.py
@@ -1,4 +1,18 @@
 #pip install gtfs partridge
 
+import partridge as ptg
+
+# Read static GTFS data
+feed = ptg.read_feed('path/to/gtfs.zip')
+
+# Access data from the feed
+routes = feed.routes
+stops = feed.stops
+trips = feed.trips
+stop_times = feed.stop_times
+
+# Example: Print the names of all routes
+for route in routes.itertuples():
+    print(route.route_short_name)
 
 #fairly certain thats the correct pip install idk i havent tested it out yet
