4a40c594c182edc422509288494f5576399a7083
r <rserrano7@ucmerced.edu>
2025-01-30 19:10:10 -0800

    Create simulink.m
    creates placeholder for future simulink usage for SLAM and SUMO simulation for real-time traffic to help with adding to the "high fidelity" of a simulation

---
+++ SIMULINK/simulink.m
@@ -0,0 +1,10 @@
+clear
+clc
+
+simulink = "i dunno"
+
+% % so from preliminary research, i CANNOT python my way out of using matlab, so im using this as a placeholder for what i need to do later
+
+% % also need to figure out how to implement data that im going to be grabbing from SLAM and SUMO, in this case will allow the data to actually be translated into a proper simulation
+
+% % tldr, simulink is gonna be able to access data and allow it be easily usable. 
